2f765522a2560dde6495b2e5f5fe1840b7e812fb
Italo Penaforte <italopenaforte@gmail.com>
2022-08-22 20:45:04 -0300

    feat: modify dispatcher to dealing with any type and build a serializer

--- src/main/java/NewOrderMain.java
+++ src/main/java/NewOrderMain.java
@@ -1,17 +1,23 @@
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (var dispatcher = new KafkaDispatcher()) {
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
 
-            for (var i = 0; i < 10; i++) {
-                var key = UUID.randomUUID().toString();
-                var value = key + ",12345,222";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                for (var i = 0; i < 10; i++) {
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+                    var order = new Order(userId, orderId, amount);
 
-                var email = "Obrigado pelo seu pedido";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+
+                    var email = "Obrigado pelo seu pedido";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
